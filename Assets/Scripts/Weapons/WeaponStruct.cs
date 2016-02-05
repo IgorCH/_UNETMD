1b376dbd986b996c64a740cd9d342869adb214ce
Igor Chernyaev <harbinger07@bk.ru>
2016-02-05 21:06:06 +0300

    hren

---
+++ Assets/Scripts/Weapons/WeaponStruct.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+namespace MostDanger {
+
+	public struct WeaponStruct {
+
+	    public string Name { get; set; }
+        public string ScriptName { get; set; }
+        public int Count { get; set; }
+
+
+	}
+
+}
