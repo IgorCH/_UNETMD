e2c1ca85123d66f8291f4c6c0b8bd75cb4e93dff
igorCH <harbinger07@bk.ru>
2016-02-05 15:20:59 +0300

    added weapons to enums

--- Assets/Scripts/Enums/WeaponType.cs
+++ Assets/Scripts/Enums/WeaponType.cs
@@ -1,10 +1,53 @@
 ﻿namespace MostDanger {
 	
 	public enum WeaponType {
-		None,
+
+		//Character
+		Fist,
+		Pistol,
+		Shotgun,
 		Rifle,
 		SniperRifle,
-		Grenade
+		Bazooka,
+		Mortar,
+		HomingMissile,
+		Grenade,
+		CassetGrenade,
+		FireCassetGrenade,
+		AcidGrenade,
+
+		HealthGrenade,
+		HealthBullet,
+
+		Jetpack,
+		Paraсhute,
+
+
+		//Airplane
+		AirplaneMachineGun,
+		AirplaneBombs,
+
+
+		//Helicopter
+		HelicopterMachineGun,
+		HelicopterBombs,
+
+		//Tank
+		TankMachineGun,
+		TankCannon,
+
+		//Turret
+		TurretMachineGun,
+		TurretCannon
+
+		//Boat
+
+
+		//Car
+
+
+		//Нужна инженерная техника
+
 	}
 
 }
