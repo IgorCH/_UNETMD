ec7c501a809353fbc80ed7f91c69998bb1d91022
igorCH <harbinger07@bk.ru>
2016-02-05 17:35:00 +0300

    continue character system

---
+++ Assets/Scripts/Managers/GnomeController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MostDanger {
+	
+	public class GnomeController : MonoBehaviour {
+
+		//protected Inventory unitInventory;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+
+		}
+	}
+
+}
