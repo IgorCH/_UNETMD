be717059ee9432adc0b9424abb8925fff3247c75
igorCH <harbinger07@bk.ru>
2016-02-05 11:14:16 +0300

    some refactoring before big work 3

---
+++ Assets/Scripts/Weapons/WeaponStruct.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+namespace MostDanger {
+
+	public struct WeaponStruct {
+
+		public string Name { set; get; }
+		public string ScriptName { set; get; }
+		public int Count { set; get; }
+
+	}
+
+}
