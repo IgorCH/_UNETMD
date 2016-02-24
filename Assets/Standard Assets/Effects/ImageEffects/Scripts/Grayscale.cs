69cfc7e3a69ee5f0fa4bac9b34af841efb38bfb2
igorCH <harbinger07@bk.ru>
2016-02-24 18:55:30 +0300

    new rosources

--- Assets/Standard Assets/Effects/ImageEffects/Scripts/Grayscale.cs
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/Grayscale.cs
@@ -1,18 +1,19 @@
+using System;
 using UnityEngine;
 
-[ExecuteInEditMode]
-[AddComponentMenu("Image Effects/Color Adjustments/Grayscale")]
-public class GrayscaleEffect : ImageEffectBase {
-  public float ramp;
+namespace UnityStandardAssets.ImageEffects
+{
+    [ExecuteInEditMode]
+    [AddComponentMenu("Image Effects/Color Adjustments/Grayscale")]
+    public class Grayscale : ImageEffectBase {
+        public Texture  textureRamp;
+        public float    rampOffset;
 
-  void OnRenderImage(RenderTexture source, RenderTexture destination) {
-    // [0, 1]
-    ramp = Mathf.Clamp(ramp, 0f, 0.75f);
-
-    // push to shader
-    material.SetFloat("_Ramp", ramp);
-
-    // magix
-    Graphics.Blit(source, destination, material);
-  }
+        // Called by camera to apply image effect
+        void OnRenderImage (RenderTexture source, RenderTexture destination) {
+            material.SetTexture("_RampTex", textureRamp);
+            material.SetFloat("_RampOffset", rampOffset);
+            Graphics.Blit (source, destination, material);
+        }
+    }
 }
