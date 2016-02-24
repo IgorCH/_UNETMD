6b8fbe81b0be297be12cd8ee6ce128ca3801da58
igorCH <harbinger07@bk.ru>
2016-02-24 16:52:50 +0300

    goblak

--- Assets/Scripts/Weapons/Character/Jetpack.cs
+++ Assets/Scripts/Weapons/Character/Jetpack.cs
@@ -5,7 +5,38 @@ namespace MostDanger {
 	
 	public class Jetpack : Weapon {
 
-		//must return default camera params
+		private void Awake()
+		{
+			//_rigidbody = GetComponent<Rigidbody> ();
+		}
+
+		private void Start()
+		{
+			
+		}
+
+		public override void ManualUpdate () {
+
+			CameraParams = new Vector3 (0, 0, 0);
+
+			if(Input.GetMouseButtonDown(0)) {
+				
+			}
+
+			if(Input.GetMouseButton(0)) {
+
+			}
+
+			if(Input.GetMouseButtonUp(0)) {
+
+			}
+
+		}
+
+		public override void SetDefaults()
+		{
+
+		}
 	}
 
 }
